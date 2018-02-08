0030a1a0de4e446a9ae6f0d8a94a0c20ea95ddec
JP Rowan <jp@Janrain-C02T30R7HP49.local>
2018-02-08 08:34:19 -0800

    added miaa as built to _Config

--- src/IdentityGroups/Families/_Config.js
+++ src/IdentityGroups/Families/_Config.js
@@ -47,3 +47,72 @@ export const FAMILY_INFO_FORM_ATTRIBUTES = [
         customValidation: null
     },
 ]
+
+// Config as defined by miaa:
+// https://miaasolutions.atlassian.net/wiki/spaces/JAO/pages/279314433/As-Built+Janrain+OEM+Internal
+export const AUTHORIZATIONS = {
+    "IS_HEAD_OF": {
+        related_users: {
+            "IS_MEMBER_OF": ['id_card'],
+            "IS_HEAD_OF": ['id_card']
+        },
+        invite_as: [],
+        can_enroll: false,
+        approve_enrollment_for: [],
+        carnet: {
+            create: ['id_card', 'familyProfile'],
+            read: ['tech', 'id_card', 'familyProfile'],
+            update: ['id_card', 'familyProfile'],
+            'delete': false
+        }
+    },
+    "IS_MEMBER_OF": {
+        related_users: {
+            "IS_MEMBER_OF": ['id_card']
+        },
+        invite_as: [],
+        can_enroll: false,
+        approve_enrollment_for: [],
+        carnet: {
+            create: [],
+            read: ['tech', 'id_card'],
+            update: [],
+            'delete': false
+        }
+    }
+}
+
+export const CARDS = {
+    "tech": [
+        "uuid",
+        "created",
+        "lastUpdated"
+    ],
+    "id_card": [
+        "familyName",
+        "description"
+    ],
+    "familyProfile": [
+        "familyName",
+        "description",
+        "familyAddress"
+    ]
+}
+
+export const SCOPES = {
+    "create": [
+        "manage_family"
+    ],
+    "read": [
+        "read_family"
+    ],
+    "edit": [
+        "manage_family"
+    ],
+    "delete": [
+        "manage_family"
+    ],
+    "invite": [
+        "manage_family"
+    ]
+}
